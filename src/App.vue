48711e7b0ea58e5ced4e3ec7bd0852db3b0be298
Abdool-Zaid <aztoorabally7447@gmail.com>
2022-09-28 14:10:39 +0200

    'link'

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,56 @@
 <template>
-  <v-app>
-    <v-app-bar
+   <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
       app
-      color="primary"
-      dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+    <v-list shaped>
+      <v-subheader>REPORTS</v-subheader>
+      <v-list-item-group
+        v-model="selectedItem"
+        color="primary"
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-list-item-icon>
+            <v-icon v-text="item.icon"></v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.text"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
     </v-app-bar>
 
     <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
-</template>
+<router-view></router-view>  
+  </v-main>
+  </v-app></template>
 
 <script>
 
 export default {
   name: 'App',
-
-  data: () => ({
-    //
+  data: () => ({ drawer: null, 
+    selectedItem: 1,
+      items: [
+        { text: 'home', icon: 'mdi-finance' },
+        { text: 'dashboard', icon: 'mdi-toolbox-outline' },
+        { text: 'user', icon: 'mdi-weight-lifter' },
+        { text: 'messages', icon: 'mdi-forum' },
+        { text: 'settings', icon: 'mdi-application-cog' },
+        { text: 'help', icon: 'mdi-help-network-outline' },
+      ],
   }),
+ 
 };
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,34 @@
 <template>
-  <hello-world />
-</template>
+  <v-flex >
+
+    <div>
+      <v-text-field
+      label="search symptoms"
+      prepend-inner-icon="mdi-store-search-outline"
+      ></v-text-field>
+    </div>
+    <div>
+      <v-text-field
+      label="search symptoms"
+      prepend-inner-icon="mdi-store-search-outline"
+      ></v-text-field>
+    </div>
+  </v-flex>
+  </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
 
   export default {
     name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
-  }
+data:()=>({
+    width:2,
+radius:10,
+padding:8,
+lineCap:'round',
+value:[0,3,4,6,3,7,8,4,4,5,6,2,7,],   
+fill:false,
+type:'trend',
+autoLineWidth:false, 
+  })
+}
 </script>
